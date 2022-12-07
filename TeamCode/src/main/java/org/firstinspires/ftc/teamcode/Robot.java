046288e70522826061fd91367fac0b7199017656
BigieCheese <89882479+PeatYT@users.noreply.github.com>
2022-12-07 09:18:01 -0600

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -25,14 +25,14 @@ public class Robot {
     public double ATmult;
 
     // driverop constants
-    public double DXmult=.5;
-    public double DYmult=.5;
-    public double DTmult=.5;
+    public double DXmult = .5;
+    public double DYmult = .5;
+    public double DTmult = .5;
 
     // for extender
     public DcMotor extender;
     public double extenderCmMultiple;
-
+    public double extendSpeed;
     // for arm
     public DcMotor shoulder;    // unused currently, may be implemented in future years
     public DcMotor elbow;
@@ -48,13 +48,13 @@ public class Robot {
 
     public Gamepad driverGamepad;
 
-    public Robot(Gamepad g, HardwareMap h, String configuration){
+    public Robot(Gamepad g, HardwareMap h, String configuration) {
         driverGamepad = g;
 
         // initializes motors
         initMotor(h);
 
-        switch(configuration){
+        switch (configuration) {
             case "basic mecanum":
                 // figure this out next year
                 break;
@@ -66,53 +66,39 @@ public class Robot {
                 BR.setDirection(DcMotor.Direction.FORWARD);
                 break;
         }
-
-
-
-
-
-
-
     }
 
-
-
-
-
-
-
-
-
-
-    void initMotor(HardwareMap hardwareMap){
+    void initMotor(HardwareMap hardwareMap) {
         FL = hardwareMap.get(DcMotor.class, "FL");
         FR = hardwareMap.get(DcMotor.class, "FR");
         BL = hardwareMap.get(DcMotor.class, "BL");
         BR = hardwareMap.get(DcMotor.class, "BR");
 
-        if(usesArm){
+        if (usesArm) {
             //figure out with arm
         }
-        if(usesExt){
-            extender=hardwareMap.get(DcMotor.class, "EXT");
+        if (usesExt) {
+            extender = hardwareMap.get(DcMotor.class, "EXT");
         }
     }
 
     //driver controlled time
-    public void driverMove(){
+    public void driverMove() {
         //units defined for readability
-        double horizontal = driverGamepad.left_stick_x*DXmult;   // this works so dont question it
-        double vertical = driverGamepad.left_stick_y*DYmult;
-        double turn = driverGamepad.right_stick_x*DTmult;
-
+        double horizontal = driverGamepad.left_stick_x * DXmult;   // this works so dont question it
+        double vertical = driverGamepad.left_stick_y * DYmult;
+        double turn = driverGamepad.right_stick_x * DTmult;
+        double extenderPosCM = extender.getCurrentPosition() * extenderCmMultiple + driverGamepad.right_stick_y * extenderCmMultiple * extendSpeed;
         //the god code. we do not change this. too much work to trial&error
         FL.setPower(Range.clip((vertical + horizontal - turn), -1, 1));
         FR.setPower(Range.clip((vertical - horizontal + turn), -1, 1));
         BL.setPower(Range.clip((vertical - horizontal - turn), -1, 1));
         BR.setPower(Range.clip((vertical + horizontal + turn), -1, 1));
+
+        extender.setTargetPosition((int) (extenderPosCM / extenderPosCM));
     }
 
-    public void runAutonomous(){
+    public void runAutonomous1() {
 
 
         //                                         //
@@ -123,46 +109,63 @@ public class Robot {
 
     }
 
-    public void autoMove(int revx, int revy){
-        FL.setTargetPosition((int) (FL.getCurrentPosition()+Range.clip((revx*AXmult + revy*AYmult), -1, 1)));
-        FR.setTargetPosition((int) (FR.getCurrentPosition()+Range.clip((revx*AXmult - revy*AYmult), -1, 1)));
-        BL.setTargetPosition((int) (BL.getCurrentPosition()+Range.clip((revx*AXmult - revy*AYmult), -1, 1)));
-        BR.setTargetPosition((int) (BR.getCurrentPosition()+Range.clip((revx*AXmult + revy*AYmult), -1, 1)));
+    public void runAutonomous2() {
+
+
+        //                                         //
+        //         INSERT AUTONOMOUS HERE          //
+        //(in terms of the functions defined below)//
+        //                                         //
+
+
     }
-    public void autoTurn(double deg){
-        FL.setTargetPosition(FL.getCurrentPosition()+Range.clip((int)(-deg*ATmult), -1, 1));
-        FR.setTargetPosition(FR.getCurrentPosition()+Range.clip((int)(deg*ATmult), -1, 1));
-        BL.setTargetPosition(BL.getCurrentPosition()+Range.clip((int)(-deg*ATmult), -1, 1));
-        BR.setTargetPosition(BR.getCurrentPosition()+Range.clip((int)(deg*ATmult), -1, 1));
+
+    public void autoMove(int revx, int revy) {
+        FL.setTargetPosition((int) (FL.getCurrentPosition() + Range.clip((revx * AXmult + revy * AYmult), -1, 1)));
+        FR.setTargetPosition((int) (FR.getCurrentPosition() + Range.clip((revx * AXmult - revy * AYmult), -1, 1)));
+        BL.setTargetPosition((int) (BL.getCurrentPosition() + Range.clip((revx * AXmult - revy * AYmult), -1, 1)));
+        BR.setTargetPosition((int) (BR.getCurrentPosition() + Range.clip((revx * AXmult + revy * AYmult), -1, 1)));
     }
-    public void extender(double CM){
-        extender.setTargetPosition((int) (CM*extenderCmMultiple));
+
+    public void autoTurn(double deg) {
+        FL.setTargetPosition(FL.getCurrentPosition() + Range.clip((int) (-deg * ATmult), -1, 1));
+        FR.setTargetPosition(FR.getCurrentPosition() + Range.clip((int) (deg * ATmult), -1, 1));
+        BL.setTargetPosition(BL.getCurrentPosition() + Range.clip((int) (-deg * ATmult), -1, 1));
+        BR.setTargetPosition(BR.getCurrentPosition() + Range.clip((int) (deg * ATmult), -1, 1));
+    }
+
+    public void extender(double CM) {
+        extender.setTargetPosition((int) (CM * extenderCmMultiple));
         extender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         extender.setPower(1);
     }
-    public void openClaw(){
+
+    public void openClaw() {
         claw.setPosition(clawOpenPos);
     }
-    public void closeClaw(){
+
+    public void closeClaw() {
         claw.setPosition(clawClosePos);
     }
-    public  void motorModeReset(){
+
+    public void motorModeReset() {
         FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
-    public void motorModeEncoder(){
+
+    public void motorModeEncoder() {
         FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    public void motorModePosition(){
+
+    public void motorModePosition() {
         FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 }
-
