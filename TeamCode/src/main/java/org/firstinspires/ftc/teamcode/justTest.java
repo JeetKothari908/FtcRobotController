044e4b32e7fea92e37b5aa4f2628f1fad1787e99
Varunv20 <varun.viges@icloud.com>
2022-12-21 17:24:25 -0600

    jiggle and move to target

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/justTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/justTest.java
@@ -28,7 +28,29 @@ public class justTest extends LinearOpMode {
                 writer.write("total of "+robot.rawJiggleData.size()+"datapoints recorded,");
                 writer.write("each represents appx. "+((30)/robot.rawJiggleData.size())+" deg of area");
                 telemetry.addLine("2");
+                double last_data = 0;
+                double main_distance;
                 for(Double d: robot.rawJiggleData) {
+                    if (last_data == 0){last_data = d;}
+                    double difference = Math.abs(d-last_data);
+                    if (difference > 0.5*d){
+                        main_distance = last_data;
+                        robot.move(0, 0, -12.5, 0, 12.05, 1);
+                        telemetry.addLine(""+main_distance);
+                        float m_distance_prime = (float) Math.sqrt(main_distance*main_distance-(200-200*Math.cos(12.5)));
+                        robot.move(0, m_distance_prime, 0, 0, 12.05, 1);
+                        // then pick up or drop cone
+
+
+                        //this is the distance of the pole/cone when outside of the sensors fov
+                        //then rotate half of fov radius- not just the sensor
+                        // if the whole robot moves
+                        // then move forward sqrt((main_distance)^2-(r2+r2﹣2r^2cosγ)) where y is 0.5 of FOV radius, and r is the distance of the sensor to its center of rotation
+                        // -- this value can be a constant we trial and error
+                        //
+                        //
+                    }
+                    last_data = d;
                     writer.write(d + System.lineSeparator());
                 }
                 telemetry.addLine("3");
